755bee211401bda13ee561b689db3c8bad2b7304
Edheramirez <edher.ramirez@outlook.es>
2020-09-30 12:41:12 -0500

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*Main - Entry Point
+*
+*Return: Alphabet without q and e
+*/
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c == 'e' || c == 'q')
+{
+c++;
+}
+putchar(c);
+c++;
+if (c > 'z')
+{
+putchar('\n');
+}
+}
+return (0);
+}
