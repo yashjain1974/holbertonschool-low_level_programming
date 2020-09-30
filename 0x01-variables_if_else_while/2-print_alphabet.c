aaada99e6f87a9e7c3172e03495e43b0f9ae3737
Edheramirez <edher.ramirez@outlook.es>
2020-09-30 11:48:46 -0500

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Return: Alphabet returns
+*
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
+}
