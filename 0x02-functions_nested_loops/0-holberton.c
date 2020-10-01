64099c27d2a44893ee85180a7be7b74e234b99ee
Edheramirez <edher.ramirez@outlook.es>
2020-10-01 16:23:04 -0500

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* main - entry point
+*
+* Return: Always ok = 0
+*/
+int main(void)
+{
+_putchar ('h');
+_putchar ('o');
+_putchar ('l');
+_putchar ('b');
+_putchar ('e');
+_putchar ('r');
+_putchar ('t');
+_putchar ('o');
+_putchar ('n');
+_putchar ('\n');
+return (0);
+}
