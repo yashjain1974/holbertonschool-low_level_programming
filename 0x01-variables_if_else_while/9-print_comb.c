ff5fc19c2d65a4dcbe5105567baa21b45fec54b3
Edheramirez <edher.ramirez@outlook.es>
2020-09-30 19:11:22 -0500

    print combin

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*Main - entry point
+*
+*Return: 0
+*/
+int main(void)
+{
+int c;
+do {
+for (c = 0; c <= 9; c++)
+{
+putchar(c + 48);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}}
+} while (c < 9);
+putchar('\n');
+return (0);
+}
