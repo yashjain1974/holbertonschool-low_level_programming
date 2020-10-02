50f2456c6e64dca8fc0a69f9ca6490390a2eb437
Edheramirez <edher.ramirez@outlook.es>
2020-10-01 23:58:40 -0500

    8 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* jack_bauer - prints the clock
+*/
+void jack_bauer(void)
+{
+int h, m, d;
+h = 0;
+while (h < 24)
+{
+m = 0;
+while (m < 60)
+{
+d = h / 10;
+_putchar('0' + d);
+d = h % 10;
+_putchar('0' + d);
+_putchar(':');
+d = m / 10;
+_putchar('0' + d);
+d = m % 10;
+_putchar('0' + d);
+_putchar('\n');
+m++;
+}
+h++;
+}
+}
